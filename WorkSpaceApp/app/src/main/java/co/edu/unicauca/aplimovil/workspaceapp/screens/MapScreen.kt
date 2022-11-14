7a3f975499f6664d4e363584bb0c49197cbde7b4
lauraich <lauraich@unicauca.edu.co>
2022-11-14 14:54:58 -0500

    Update map functions

--- WorkSpaceApp/app/src/main/java/co/edu/unicauca/aplimovil/workspaceapp/screens/MapScreen.kt
+++ WorkSpaceApp/app/src/main/java/co/edu/unicauca/aplimovil/workspaceapp/screens/MapScreen.kt
@@ -4,7 +4,7 @@ import android.Manifest
 import android.app.Activity
 import android.content.pm.PackageManager
 import android.location.Location
-import android.widget.Toast
+import android.location.LocationManager
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -22,9 +22,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
-import androidx.core.app.ActivityCompat.requestPermissions
-import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
-import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.navigation.NavController
@@ -32,17 +30,13 @@ import co.edu.unicauca.aplimovil.workspaceapp.models.Place
 import co.edu.unicauca.aplimovil.workspaceapp.navigation.AppScreens
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.Priority.PRIORITY_BALANCED_POWER_ACCURACY
+import com.google.android.gms.location.*
 import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.GoogleMapOptions
-import com.google.android.gms.maps.MapsInitializer
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
 import com.google.maps.android.compose.*
+
 
 private var lati:Double=0.0
 private var longi:Double=0.0
@@ -88,7 +82,7 @@ fun MapBodyContent(navController: NavController?) {
     ) {
         Text(
             text = "¡Encuentra espacios de trabajo cercanos!",
-            modifier = Modifier.padding(top = 5.dp)
+            modifier = Modifier.padding(top = 10.dp)
         )
         checkLocationPermissions(navController)
     }
@@ -126,34 +120,44 @@ private fun checkLocationPermissions(navController: NavController?) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+          val allPermissionsRevoked =
+              permissionStates.permissions.size ==
+                      permissionStates.revokedPermissions.size
+          if (!allPermissionsRevoked) {
+              if(isLocationEnabled()){
+                  var location =rememberSaveable {
+                      mutableStateOf(Location(null))
+                  }
+                  getLocation(onValueChange = {
+                      location.value =it })
+                  //Text("Permisos dados")
+                  if(location.value.latitude!=0.0 && location.value.longitude!=0.0){
+                      var placeList: MutableList<Place>
+                      placeList=listOf<Place>(Place("dasd","fsdf","fsdfsd","sdfs",2.4419417,-76.6092564,"fsd"),Place("","","","",2.4416987,-76.6060809,"")) as MutableList<Place>
+                      //placeList = nearPlaces(location = location.value,placeList)
+                      Mapa(lat = location.value.latitude, long = location.value.longitude,placeList,navController!!)
+                  }
+              }else{
+                  dialogMessage(openDialog = openDialog, text = "Habilita la ubicación de tu dispositivo desde configuraciones",navController)
+              }
 
-        val allPermissionsRevoked =
-            permissionStates.permissions.size ==
-                    permissionStates.revokedPermissions.size
-        println(allPermissionsRevoked)
-        if (!allPermissionsRevoked) {
-            var location =rememberSaveable {
-                mutableStateOf(Location(null))
-            }
-            getLocation(onValueChange = {
-                location.value =it })
-            //Text("Permisos dados")
-            if(location.value.latitude!=0.0 && location.value.longitude!=0.0){
+          } else if (permissionStates.shouldShowRationale) {
+              //dialogMessage(openDialog = openDialog, text = "Por favor cambia la configuracion de la ubicacion a una más exacta")
 
-                Mapa(lat = location.value.latitude, long = location.value.longitude,nearPlaces(location = location.value),navController!!)
-            }
-        } else if (permissionStates.shouldShowRationale) {
-            dialogMessage(openDialog = openDialog, text = "Por favor cambia la configuracion de la ubicacion a una más exacta")
+          } else {
+              if(permissionStates.revokedPermissions.size==2){
+                  dialogMessage(openDialog = openDialog, text = "Para acceder a esta funcionalidad permite el uso de la ubicación desde la configuracion de tu dispositivo",navController)
 
-        } else {
-            dialogMessage(openDialog = openDialog, text = "Por favor permite el uso de la ubicación desde la configuracion de tu dispositivo")
-        }
+              }
+          }
+
     }
 }
 
 @Composable
 fun getLocation(onValueChange:(Location)->Unit){
     val fusedLocationClient = LocationServices.getFusedLocationProviderClient(LocalContext.current)
+
     if (ActivityCompat.checkSelfPermission(
             LocalContext.current,
             Manifest.permission.ACCESS_FINE_LOCATION
@@ -171,11 +175,22 @@ fun getLocation(onValueChange:(Location)->Unit){
             101
         )
     }
-    fusedLocationClient.getCurrentLocation(PRIORITY_BALANCED_POWER_ACCURACY,null).addOnSuccessListener {
-        onValueChange(it)
-    }
+
+        fusedLocationClient.getCurrentLocation(PRIORITY_HIGH_ACCURACY,null).addOnSuccessListener {
+            onValueChange(it)
+        }
+
 
 }
+    @Composable
+    fun isLocationEnabled():Boolean{
+        val mLocationManager = getSystemService(LocalContext.current,LocationManager::class.java) as LocationManager
+
+        // Checking GPS is enabled
+        val mGPS = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        return mGPS
+    }
+
 
 @Composable
 fun Mapa(lat:Double,long:Double,nearPlaces:MutableList<Place>,navController: NavController){
@@ -185,8 +200,9 @@ fun Mapa(lat:Double,long:Double,nearPlaces:MutableList<Place>,navController: Nav
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(location, 15F)
     }
+
     GoogleMap(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize().padding(top=15.dp),
         cameraPositionState = cameraPositionState
     ) {
        Marker(state=MarkerState(position = location),icon= BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN), title = "Te encuentras aquí")
@@ -199,12 +215,12 @@ fun Mapa(lat:Double,long:Double,nearPlaces:MutableList<Place>,navController: Nav
                onInfoWindowClick = { navController.navigate(route = AppScreens.DetailScreen.route)}
            )
        }
-
     }
+
 }
 
 @Composable
-fun dialogMessage(openDialog:MutableState<Boolean>,text:String){
+fun dialogMessage(openDialog:MutableState<Boolean>,text:String,navController: NavController?){
 
     if (openDialog.value) {
         AlertDialog(
@@ -234,6 +250,7 @@ fun dialogMessage(openDialog:MutableState<Boolean>,text:String){
             dismissButton = {
                 TextButton(
                     onClick = {
+                        navController?.popBackStack()
                         openDialog.value = false
                     }
                 ) {
@@ -244,8 +261,7 @@ fun dialogMessage(openDialog:MutableState<Boolean>,text:String){
     }
 }
 
-fun nearPlaces(location:Location):MutableList<Place>{
-    var listPlaces :MutableList<Place> = mutableListOf()
+fun nearPlaces(location:Location,listPlaces:MutableList<Place>):MutableList<Place>{
     var resPlaces :MutableList<Place> = mutableListOf()
     listPlaces.add(Place("dasd","fsdf","fsdfsd","sdfs",2.4419417,-76.6092564,"fsd"))
     for (item in listPlaces){
